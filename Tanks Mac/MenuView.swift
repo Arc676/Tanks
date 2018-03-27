8fda28647031203dcd46614c6bbcde620b6a9032
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-03-27 14:47:54 +0800

    Implemented player loading in MenuView
    Removed path element for persistence UI

--- Tanks Mac/MenuView.swift
+++ Tanks Mac/MenuView.swift
@@ -27,13 +27,15 @@ class MenuView : NSView {
 
 	@IBOutlet weak var terrainSelection: NSButton!
 	var terrainType: TerrainType = .RANDOM
+	var players: [Tank?] = [
+		nil, nil, nil, nil
+	]
 
 	//player 1
 	@IBOutlet weak var p1name: NSTextField!
 	@IBOutlet weak var p1color: NSColorWell!
 	@IBOutlet weak var p1isAI: NSButton!
 	@IBOutlet weak var p1AILvl: NSSegmentedControl!
-	@IBOutlet weak var p1Path: NSPathControl!
 	@IBOutlet weak var p1Load: NSButton!
 	@IBOutlet weak var p1Unload: NSButton!
 
@@ -42,7 +44,6 @@ class MenuView : NSView {
 	@IBOutlet weak var p2color: NSColorWell!
 	@IBOutlet weak var p2isAI: NSButton!
 	@IBOutlet weak var p2AILvl: NSSegmentedControl!
-	@IBOutlet weak var p2Path: NSPathControl!
 	@IBOutlet weak var p2Load: NSButton!
 	@IBOutlet weak var p2Unload: NSButton!
 
@@ -52,7 +53,6 @@ class MenuView : NSView {
 	@IBOutlet weak var p3color: NSColorWell!
 	@IBOutlet weak var p3isAI: NSButton!
 	@IBOutlet weak var p3AILvl: NSSegmentedControl!
-	@IBOutlet weak var p3Path: NSPathControl!
 	@IBOutlet weak var p3Load: NSButton!
 	@IBOutlet weak var p3Unload: NSButton!
 
@@ -62,26 +62,27 @@ class MenuView : NSView {
 	@IBOutlet weak var p4color: NSColorWell!
 	@IBOutlet weak var p4isAI: NSButton!
 	@IBOutlet weak var p4AILvl: NSSegmentedControl!
-	@IBOutlet weak var p4Path: NSPathControl!
 	@IBOutlet weak var p4Load: NSButton!
 	@IBOutlet weak var p4Unload: NSButton!
 
 	@IBAction func togglePlayer(_ sender: NSButton) {
 		let state = sender.state == NSControl.StateValue.on
 		if sender == enableP3 {
+			let loadState = players[2] == nil
 			p3name.isEnabled = state
 			p3color.isEnabled = state
 			p3isAI.isEnabled = state
 			p3AILvl.isEnabled = state && p3isAI.state == NSControl.StateValue.on
-			p3Load.isEnabled = state
-			p3Unload.isEnabled = state
+			p3Load.isEnabled = state && loadState
+			p3Unload.isEnabled = state && !loadState
 		} else {
+			let loadState = players[3] == nil
 			p4name.isEnabled = state
 			p4color.isEnabled = state
 			p4isAI.isEnabled = state
 			p4AILvl.isEnabled = state && p4isAI.state == NSControl.StateValue.on
-			p4Load.isEnabled = state
-			p4Unload.isEnabled = state
+			p4Load.isEnabled = state && loadState
+			p4Unload.isEnabled = state && !loadState
 		}
 	}
 
@@ -99,11 +100,61 @@ class MenuView : NSView {
 	}
 	
 	@IBAction func loadFromFile(_ sender: NSButton) {
-		//
+		var failed = false
+		let panel = NSOpenPanel()
+		if panel.runModal() == NSApplication.ModalResponse.OK {
+			do {
+				let data = try Data(contentsOf: panel.url!)
+				if let tank = NSKeyedUnarchiver.unarchiveObject(with: data) as? Tank {
+					if sender == p1Unload {
+						players[0] = tank
+						p1Unload.isEnabled = true
+						p1Load.isEnabled = false
+					} else if sender == p2Unload {
+						players[1] = tank
+						p2Unload.isEnabled = true
+						p2Load.isEnabled = false
+					} else if sender == p3Unload {
+						players[2] = tank
+						p3Unload.isEnabled = true
+						p3Load.isEnabled = false
+					} else {
+						players[3] = tank
+						p4Unload.isEnabled = true
+						p4Load.isEnabled = false
+					}
+				} else {
+					failed = true
+				}
+			} catch {
+				failed = true
+			}
+			if failed {
+				let alert = NSAlert()
+				alert.messageText = "Failed to load tank data"
+				alert.runModal()
+			}
+		}
 	}
 
 	@IBAction func unloadFile(_ sender: NSButton) {
-		//
+		if sender == p1Unload {
+			players[0] = nil
+			p1Unload.isEnabled = false
+			p1Load.isEnabled = true
+		} else if sender == p2Unload {
+			players[1] = nil
+			p2Unload.isEnabled = false
+			p2Load.isEnabled = true
+		} else if sender == p3Unload {
+			players[2] = nil
+			p3Unload.isEnabled = false
+			p3Load.isEnabled = true
+		} else {
+			players[3] = nil
+			p4Unload.isEnabled = false
+			p4Load.isEnabled = true
+		}
 	}
 
 	@IBAction func selectTerrain(_ sender: NSButton) {
@@ -122,16 +173,27 @@ class MenuView : NSView {
 	}
 
 	@IBAction func startGame(_ sender: Any) {
-		var players: [Tank] = []
-		players.append(createTank(p1name.stringValue, p1color.color, 1, isCC: p1isAI, aiLvl: p1AILvl))
-		players.append(createTank(p2name.stringValue, p2color.color, 2, isCC: p2isAI, aiLvl: p2AILvl))
-		if (p3name.isEnabled) {
-			players.append(createTank(p3name.stringValue, p3color.color, 3, isCC: p3isAI, aiLvl: p3AILvl))
+		if players[0] == nil {
+			players[0] = createTank(p1name.stringValue, p1color.color, 1, isCC: p1isAI, aiLvl: p1AILvl)
 		}
-		if (p4name.isEnabled) {
-			players.append(createTank(p4name.stringValue, p4color.color, 4, isCC: p4isAI, aiLvl: p4AILvl))
+		if players[1] == nil {
+			players[1] = createTank(p2name.stringValue, p2color.color, 2, isCC: p2isAI, aiLvl: p2AILvl)
 		}
-		viewController?.initialize(terrainType: terrainType, players: players)
+		if p3name.isEnabled {
+			if players[2] == nil {
+				players[2] = createTank(p3name.stringValue, p3color.color, 3, isCC: p3isAI, aiLvl: p3AILvl)
+			}
+		} else {
+			players[2] = nil
+		}
+		if p4name.isEnabled {
+			if players[3] == nil {
+				players[3] = createTank(p4name.stringValue, p4color.color, 4, isCC: p4isAI, aiLvl: p4AILvl)
+			}
+		} else {
+			players[3] = nil
+		}
+		viewController?.initialize(terrainType: terrainType, players: players.flatMap{ $0 })
 		viewController?.startGame()
 	}
 
